98d9a8a32cc3f0d048488cc01c871717869b88b5
Wouter de Bruijn <wouter@hedium.nl>
2024-12-04 09:27:25 +0100

    ♻️ Cleaned up Git util, adding branch options

--- utils/git/git.py
+++ utils/git/git.py
@@ -10,6 +10,10 @@ class Git:
     author = pygit2.Signature("Zabbix Configuration", "zabbix@example.com")
 
     def __init__(self, path: str = None):
+        """
+        Initialize git repository
+        :param path: Path to the git repository, defaults to ./cache
+        """
         if not path:
             path = "./cache"
 
@@ -23,14 +27,44 @@ class Git:
 
     @property
     def has_changes(self):
+        """
+        Check if the repository has changes, returns True if there are changes, False otherwise
+        """
         return len(self.repository.status()) > 0
 
+    def switch_branch(self, branch: str):
+        """
+        Switch to a branch, if the branch does not exist, create it
+        """
+        if branch not in self.repository.branches.local:
+            self.create_branch(branch)
+
+        local_branch = self.repository.branches.local[branch]
+
+        self.repository.checkout(local_branch)
+
+    def create_branch(self, branch: str):
+        """
+        Create a branch
+        """
+        try:
+            self.repository.branches.local.create(
+                branch, self.repository.head.peel())
+        except Exception as e:
+            logger.error(f"Failed to create branch: {e}")
+
     def add_all(self):
+        """
+        Add all changes to the index
+        """
         index = self.repository.index
         index.add_all()
         index.write()
 
     def commit(self, message: str):
+        """
+        Commit current index to the repository
+        """
         index = self.repository.index
         index.write()
 
@@ -45,12 +79,18 @@ class Git:
             [self.repository.head.target] if not self.repository.head_is_unborn else []
         )
 
-    def push(self, remote_url: str, credentials):
+    def push(self, remote_url: str, credentials, branch: str = None):
+        """
+        Push the changes to the remote repository
+        """
         remote = self.repository.remotes['origin']
+
+        if not branch:
+            branch = self.repository.head.shorthand
 
         if not remote:
             remote = self.repository.remotes.create('origin', remote_url)
 
         callbacks = pygit2.RemoteCallbacks(credentials=credentials)
 
-        remote.push(["refs/heads/main"], callbacks=callbacks)
+        remote.push([f"refs/heads/{branch}"], callbacks=callbacks)
