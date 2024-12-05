c48461e399df599350c65cb8265806678ab932b8
Wouter de Bruijn <wouter@hedium.nl>
2024-12-05 11:12:07 +0100

    ✨Added import logic

--- utils/zabbix/zabbix.py
+++ utils/zabbix/zabbix.py
@@ -1,5 +1,9 @@
 from zabbix_utils import ZabbixAPI
 from typing import ParamSpec
+from ruamel.yaml import YAML
+from io import StringIO
+
+yaml = YAML()
 
 P = ParamSpec('P')
 
@@ -34,5 +38,32 @@ class Zabbix():
                     "templates": template_ids
                 },
                 "format": "yaml"
+            }
+        )['result']
+
+    def import_template(self, template: dict):
+        reconstructed_export = StringIO()
+
+        yaml.dump({
+            "zabbix_export": {
+                "templates": [template],
+                "version": template.pop('synchronization_zabbix_version')
+            }
+        }, reconstructed_export)
+
+        with open("./tests/export.yaml", "w") as file:
+            file.write(reconstructed_export.getvalue())
+
+        return self.zapi.send_api_request(
+            "configuration.import",
+            {
+                "format": "yaml",
+                "rules": {
+                    "templates": {
+                        "createMissing": True,
+                        "updateExisting": True
+                    }
+                },
+                "source": reconstructed_export.getvalue()
             }
         )['result']
