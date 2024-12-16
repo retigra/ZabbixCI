1fb8186a8e767a885bc368bb132af7392b604fb5
Wouter de Bruijn <wouter@hedium.nl>
2024-12-16 09:35:28 +0100

    ✨ Added cli helper

---
+++ cli.py
@@ -0,0 +1,63 @@
+import argparse
+import settings
+
+import logging
+import main
+import dotenv
+
+# Read command line arguments to fill the settings
+
+
+def read_args():
+    parser = argparse.ArgumentParser(description="Zabbix to Git")
+    parser.add_argument(
+        'action',
+        help="The action to perform",
+        choices=["push", "pull"],
+    )
+    parser.add_argument(
+        "--remote",
+        help="The remote repository to push to",
+        default=settings.REMOTE,
+    )
+    parser.add_argument(
+        "--cache",
+        help="The cache path to store the Zabbix templates",
+        default=settings.CACHE_PATH,
+    )
+    parser.add_argument(
+        "--debug",
+        help="Enable debug logging",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--dotenv",
+        help="Path to the .env file",
+        default=".env",
+    )
+    return parser.parse_args()
+
+
+def parse_cli():
+    args = read_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    settings.REMOTE = args.remote
+    settings.CACHE_PATH = args.cache
+
+    if args.dotenv:
+        dotenv.load_dotenv(args.dotenv)
+
+    if args.action == "push":
+        main.push()
+
+    elif args.action == "pull":
+        main.pull()
+
+
+if __name__ == "__main__":
+    parse_cli()
