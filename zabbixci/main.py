87436d632489f6097af678028c953aa505ed15fd
Wouter de Bruijn <wouter@hedium.nl>
2025-01-03 11:03:33 +0100

    🔊 Changed brackets

--- zabbixci/main.py
+++ zabbixci/main.py
@@ -65,7 +65,7 @@ def zabbix_to_file(cache_path=Settings.CACHE_PATH):
 
     for index, batch in enumerate(batches):
         logger.info(
-            f"Processing export batch {index + 1}/{len(batches)} ({index * Settings.BATCH_SIZE}/{len(templates)})"
+            f"Processing export batch {index + 1}/{len(batches)} [{index * Settings.BATCH_SIZE}/{len(templates)}]"
         )
 
         # Get the templates
