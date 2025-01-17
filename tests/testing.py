8d6fde801c05ec1ec76d51c652c680dde26c78ab
Wouter de Bruijn <wouter@hedium.nl>
2025-01-17 14:31:07 +0100

    ✅ Added basic function tests

--- tests/testing.py
+++ tests/testing.py
@@ -8,7 +8,7 @@ from zabbixci.settings import Settings
 
 DEV_ZABBIX_URL = getenv("ZABBIX_URL")
 DEV_ZABBIX_TOKEN = getenv("ZABBIX_TOKEN")
-DEV_GIT_REMOTE = getenv("GIT_REMOTE")
+DEV_GIT_REMOTE = getenv("REMOTE")
 
 
 class TestPushFunctions(unittest.IsolatedAsyncioTestCase):
@@ -17,28 +17,31 @@ class TestPushFunctions(unittest.IsolatedAsyncioTestCase):
             ZabbixCI.cleanup_cache(full=True)
 
         logging.basicConfig(
-            format="%(asctime)s [%(name)s]  [%(levelname)s]: %(message)s",
-            level=logging.INFO,
+            level=logging.ERROR,
+            format="%(asctime)s - %(name)s - %(message)s",
         )
-
-        zabbixci_logger = logging.getLogger("zabbixci")
-        zabbixci_logger.setLevel(logging.DEBUG)
 
         Settings.ZABBIX_URL = DEV_ZABBIX_URL
         Settings.ZABBIX_TOKEN = DEV_ZABBIX_TOKEN
         Settings.REMOTE = DEV_GIT_REMOTE
-        Settings.TEMPLATE_WHITELIST = [
-            "Linux by Zabbix agent",
-            "Windows by Zabbix agent moved",
-            "Acronis Cyber Protect Cloud by HTTP",
-            "Kubernetes API server by HTTP",
-            "Kubernetes cluster state by HTTP",
-        ]
+        Settings.TEMPLATE_WHITELIST = "Linux by Zabbix agent,Linux by Zabbix 00000,Windows by Zabbix agent,Acronis Cyber Protect Cloud by HTTP,Kubernetes API server by HTTP,Kubernetes cluster state by HTTP"
 
         self.zci = ZabbixCI()
 
+    async def restoreState(self):
+        ZabbixCI.cleanup_cache()
+
+        # Restore Zabbix to initial testing state
+        Settings.PULL_BRANCH = "test"
+        await self.zci.pull()
+
+        Settings.PULL_BRANCH = "main"
+        await self.zci.push()
+
     async def asyncSetUp(self):
         await self.zci.create_zabbix()
+
+        await self.restoreState()
 
     async def test_push_pull_remote_defaults(self):
         # Push default Zabbix templates to remote
@@ -50,63 +53,92 @@ class TestPushFunctions(unittest.IsolatedAsyncioTestCase):
 
     async def test_template_change(self):
         # Rename a template
-        id = self.zci._zabbix.get_templates_name(["Linux by Zabbix agent"])[0][
+        id = self.zci._zabbix.get_templates_name(["Windows by Zabbix agent"])[0][
             "templateid"
         ]
-        self.zci._zabbix.set_template(id, {"name": "Linux by Zabbix agent (renamed)"})
+        self.zci._zabbix.set_template(id, {"name": "Windows by Zabbix agent (renamed)"})
 
         # Push changes to git
         changed = await self.zci.push()
-        self.assertTrue(changed)
+        self.assertTrue(changed, "Template change not detected")
 
-        # Make changes in Zabbix
-        self.zci._zabbix.set_template(id, {"name": "Linux by Zabbix agent"})
+        # Revert changes in Zabbix
+        self.zci._zabbix.set_template(id, {"name": "Windows by Zabbix agent"})
 
         # Restore to Git version
         changed = await self.zci.pull()
-        self.assertTrue(changed)
+        self.assertTrue(changed, "Template was not restored")
 
-        # Assert Git version is restored
+        # Assert Git version is imported back into Zabbix
         matches = self.zci._zabbix.get_templates_filtered(
-            [Settings.ROOT_TEMPLATE_GROUP], ["Linux by Zabbix agent"]
+            [Settings.ROOT_TEMPLATE_GROUP], ["Windows by Zabbix agent"]
         )
-        self.assertEqual(len(matches), 1)
-        self.assertEqual(matches[0]["name"], "Linux by Zabbix agent (renamed)")
+        self.assertEqual(len(matches), 1, "Template not found")
+        self.assertEqual(
+            matches[0]["name"],
+            "Windows by Zabbix agent (renamed)",
+            "Template name not restored",
+        )
 
-        # Restore original name
-        self.zci._zabbix.set_template(id, {"name": "Linux by Zabbix agent"})
-        changed = await self.zci.push()
-        self.assertTrue(changed)
+        await self.restoreState()
 
     async def test_template_rename(self):
         # Rename a template
         id = self.zci._zabbix.get_templates_name(["Linux by Zabbix agent"])[0][
             "templateid"
         ]
-        self.zci._zabbix.set_template(id, {"host": "Linux by Zabbix agent moved"})
+        self.zci._zabbix.set_template(id, {"host": "Linux by Zabbix 00000"})
 
         # Push changes to git
         changed = await self.zci.push()
-        self.assertTrue(changed)
+        self.assertTrue(changed, "Renaming not detected")
 
         # Make changes in Zabbix
         self.zci._zabbix.set_template(id, {"host": "Linux by Zabbix agent"})
 
         # Restore to Git version
         changed = await self.zci.pull()
-        self.assertTrue(changed)
+        self.assertTrue(changed, "Template was not restored")
 
         # Assert Git version is restored
         matches = self.zci._zabbix.get_templates_filtered(
-            [Settings.ROOT_TEMPLATE_GROUP], ["Linux by Zabbix agent"]
+            [Settings.ROOT_TEMPLATE_GROUP], ["Linux by Zabbix 00000"]
         )
-        self.assertEqual(len(matches), 1)
-        self.assertEqual(matches[0]["host"], "Linux by Zabbix agent moved")
+        self.assertEqual(len(matches), 1, "Template not found")
 
-        # Restore original name
-        self.zci._zabbix.set_template(id, {"host": "Linux by Zabbix agent"})
+        await self.restoreState()
+
+    async def test_template_delete(self):
+        # Delete a template
+        id = self.zci._zabbix.get_templates_name(
+            ["Acronis Cyber Protect Cloud by HTTP"]
+        )[0]["templateid"]
+        self.zci._zabbix.delete_template([id])
+
+        # Push changes to git
         changed = await self.zci.push()
-        self.assertTrue(changed)
+        self.assertTrue(changed, "Deletion from Zabbix not detected")
+
+        Settings.PULL_BRANCH = "test"
+
+        changed = await self.zci.pull()
+        self.assertTrue(changed, "Template was not restored")
+
+        Settings.PULL_BRANCH = "main"
+        changed = await self.zci.pull()
+        self.assertTrue(changed, "Template deletion from Git was not detected")
+
+        await self.restoreState()
+
+    async def test_push_to_new_branch(self):
+        Settings.PUSH_BRANCH = "new-branch"
+
+        # Push default Zabbix templates to remote
+        await self.zci.push()
+
+        Settings.PUSH_BRANCH = "main"
+
+        await self.restoreState()
 
     async def asyncTearDown(self):
         await self.zci._zabbix.zapi.logout()
