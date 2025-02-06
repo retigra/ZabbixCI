e5bdeeaaf4f40aa5a7d8c530d89b6df0b3523159
Wouter de Bruijn <wouter@hedium.nl>
2025-02-06 15:24:55 +0100

    🐛 Changed logging for name display in debug mode

--- zabbixci/logging.py
+++ zabbixci/logging.py
@@ -1,25 +1,32 @@
 import logging
 
+from zabbixci.settings import Settings
+
+grey = "\x1b[38;20m"
+yellow = "\x1b[33;20m"
+red = "\x1b[31;20m"
+bold_red = "\x1b[31;1m"
+reset = "\x1b[0m"
+
 
 class CustomFormatter(logging.Formatter):
 
-    grey = "\x1b[38;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
-    format = "%(asctime)s  [%(levelname)s]: %(message)s"
-    format_debug = "%(asctime)s [%(name)s]  [%(levelname)s]: %(message)s"
+    log_format = "%(asctime)s  [%(levelname)s]: %(message)s"
+    formats = {}
 
-    FORMATS = {
-        logging.DEBUG: grey + format_debug + reset,
-        logging.INFO: grey + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset,
-    }
+    def __init__(self):
+        if Settings.DEBUG or Settings.DEBUG_ALL:
+            self.log_format = "%(asctime)s [%(name)s]  [%(levelname)s]: %(message)s"
+
+        self.formats = {
+            logging.DEBUG: grey + self.log_format + reset,
+            logging.INFO: grey + self.log_format + reset,
+            logging.WARNING: yellow + self.log_format + reset,
+            logging.ERROR: red + self.log_format + reset,
+            logging.CRITICAL: bold_red + self.log_format + reset,
+        }
 
     def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
+        log_fmt = self.formats.get(record.levelno)
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
